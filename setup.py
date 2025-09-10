f93fe1eafd1e3a4eb608320dcbb3aca67febb2f9
Roberto Di Rosa <dirosa.2043388@studenti.uniroma1.it>
2025-09-10 15:38:18 +0200

    Removed setup.py and corresponding modules

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("heapitem.pyx", language_level=3,
-    compiler_directives={'boundscheck': False, 'wraparound': False})
-)
